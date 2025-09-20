fdc4fa0717b4b1a54dc66e8d6c3b97f2436e7981
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-21 00:31:29 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-rsync -avz --delete ./static_site/ ubuntu@<EC2_PUBLIC_IP>:/var/www/mysite/
-ssh ubuntu@<EC2_PUBLIC_IP> "sudo systemctl reload nginx"
+rsync -avz --delete -e "ssh -i ~/.ssh/aws-key.pem" ./static_site/ ubuntu@<EC2_PUBLIC_IP>:/var/www/mysite
+ssh -i ~/.ssh/aws-key.pem ubuntu@<EC2_PUBLIC_IP> "sudo systemctl reload nginx"
+
+echo "Static site deployment is done!"
+exit 0
