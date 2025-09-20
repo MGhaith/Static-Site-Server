2c5297c2f5ea26effd78841a6b3e00bdb54c15e9
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-21 00:16:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avz --delete ./static_site/ ubuntu@<EC2_PUBLIC_DNS>:/var/www/mysite
-ssh ubuntu@<EC2_PUBLIC_DNS> "sudo systemctl reload nginx"
+rsync -avz --delete ./static_site/ ubuntu@<EC2_PUBLIC_IP>:/var/www/mysite/
+ssh ubuntu@<EC2_PUBLIC_IP> "sudo systemctl reload nginx"
